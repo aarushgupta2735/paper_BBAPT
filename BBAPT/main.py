Fixed the action softmax problems

BBAPT/main.py
@@ -14,6 +14,10 @@ from BBAPT.src.behavioral_map import apply_behavioural_mapping
 from BBAPT.src.data_prep import data_for_timesnet, data_prep
 from BBAPT.src.portfolio_env import PortfolioEnv
 from BBAPT.src.gridsearch import run_grid_search, make_timesnet_forecast_fn
+from BBAPT.src.a2c import PortfolioActorCriticPolicy
+import torch.nn as nn
+
+
 
 def init_wandb_run(config):
     try:
@@ -47,28 +51,8 @@ def main():
         allow_short_selling=False,
         technical_indicator_list=["MACD", "RSI_14", "CCI_20", "SMA_20", "EMA_20"],
         transaction_cost=0.001,
-        ticker_list=[
-            "BTC-USD",
-            "ETH-USD",
-            "LTC-USD",
-            "LINK-USD",
-            "BCH-USD",
-            "UNI-USD",
-            "XLM-USD",
-            "FIL-USD",
-            "BNB-USD",
-            "SOL-USD",
-            "XRP-USD",
-            "ADA-USD",
-            "SHIB-USD",
-            "TON-USD",
-            "DOGE-USD",
-            "AVAX-USD",
-            "TRX-USD",
-            "DOT-USD",
-            "MATIC-USD",
-            "ETC-USD",
-        ],
+        ticker_list=["BTC-USD", "ETH-USD", "LTC-USD", "LINK-USD", "BCH-USD", "UNI-USD", "XLM-USD", "FIL-USD", "BNB-USD", "SOL-USD", "XRP-USD", "ADA-USD", "SHIB-USD", "TON-USD", "DOGE-USD", "AVAX-USD", "TRX-USD", "DOT-USD", "MATIC-USD", "ETC-USD"],
+
         train_starting_date="2020-09-22",
         train_ending_date="2022-06-07",
         test_starting_date="2022-06-08",
@@ -134,7 +118,7 @@ def main():
             },
         )
 
-        env = PortfolioEnv(data=train_df, config=config)
+        env = PortfolioEnv(data=train_df, config=config, already_weight = False)
         try:
             check_env(env.unwrapped)
             print("Environment passes all checks!")
@@ -147,8 +131,14 @@ def main():
             )
 
         print("--- Training RL Model ---")
+    
         train_started_at = perf_counter()
-        model_rl = A2C("MlpPolicy", env, verbose=1)
+        model_rl = A2C(PortfolioActorCriticPolicy, env, verbose=1,
+            policy_kwargs=dict(
+            net_arch=dict(pi=[320, 160, 80], vf=[64, 16, 4]),  # paper's Table 5, optional
+            activation_fn=nn.Tanh,                              # paper's shared-trunk activation
+        ),)
+
         model_rl.learn(total_timesteps=10000)
         train_duration_seconds = perf_counter() - train_started_at
         print("Training completed!")
@@ -193,7 +183,7 @@ def main():
         print(f"ra_n = {config.ra_n}")
 
         print("--- Running Inference ---")
-        test_env = PortfolioEnv(data=test_df, config=config)
+        test_env = PortfolioEnv(data=test_df, config=config, already_weight = True)
 
         obs, info = test_env.reset()
         done = False
@@ -201,8 +191,8 @@ def main():
 
         while not done:
             action, _states = model_rl.predict(obs)
-            forecast = nf.predict(df=data_for_timesnet(test_df[test_df["ds"] <= date]))
-            avg_return = forecast["TimesNet"].mean()
+            forecast = nf.predict(df=data_for_timesnet(test_df[test_df["ds"] <= date])) #TODO: compute once outside loop and then use 
+            avg_return = forecast["TimesNet"].mean() 
             action = apply_behavioural_mapping(action, avg_return, test_df, date, config)
             obs, reward, done, truncated, info = test_env.step(action)
             date = info["date"]

BBAPT/src/portfolio_env.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.special import softmax
 
 class PortfolioEnv(gym.Env):
-    def __init__(self, data, config):
+    def __init__(self, data, config, already_weight):
         self.data = data
         self.ticker_list = config.ticker_list
         self.n_assets = len(config.ticker_list)
@@ -25,6 +25,7 @@ class PortfolioEnv(gym.Env):
         self.ra_k_loss = config.ra_k_loss
         self.ra_k_gain = config.ra_k_gain
         self.ra_n = config.ra_n
+        self.already_weight = already_weight 
 
         self.sharpe_ratio_window = config.sharpe_ratio_window
         self.returns_window = np.full((config.sharpe_ratio_window,),np.nan)
@@ -53,6 +54,9 @@ class PortfolioEnv(gym.Env):
         return current_row[features].to_numpy(dtype=np.float32)
 
     def step(self, action):
+        if(not self.already_weight):
+            action = softmax(action)
+
         next_date = self.dates[self.current_step+1]
         current_row = self.data[self.data['ds'] == next_date].sort_values('unique_id')
         current_return = current_row['y'].to_numpy(dtype=np.float32)
@@ -66,23 +70,23 @@ class PortfolioEnv(gym.Env):
         
         self.rolling_sharpe_ratio = mean/std if std!=0 else 0 
         self.sharpe_ratio = self.profits.mean()/self.profits.std() if self.profits.std()!=0 else 0
-        self.FCR *= (1+profit)
-        self.ARR = ((self.balance/self.initial_balance)**(1/self.current_step))-1
-        self.weights = np.add(self.weights,action)
+
 
         reward = self.rolling_sharpe_ratio
         self.balance += profit
         self.current_step += 1
+        self.FCR *= (1+profit)
+        self.ARR = ((self.balance/self.initial_balance)**(1/self.current_step))-1
+        self.weights = np.add(self.weights,action)
         done = self.current_step>= self.n_steps
 
         if not done:
-            self.weights = np.normalize(self.weights)
             self.date = self.dates[self.current_step]
             next_observation = self._get_observation(self.date)
         else:
             self.FCR -= 1
-            self.AV = std*sqrt(252)
-            self.weights = np.normalize(self.weights)
+            self.AV = std*np.sqrt(252)
+            self.weights = np.softmax(self.weights)
             next_observation = np.zeros((self.n_assets, 1 + self.n_technical_indicators), dtype=np.float32)
 
         self.prev_action = action
@@ -95,9 +99,16 @@ class PortfolioEnv(gym.Env):
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
+        self.rolling_sharpe_ratio = 0.0 # for reward
+        self.sharpe_ratio = 0.0 # for inference and comparision
+        self.FCR = 1
+        self.ARR = 0.0 #can be computed in the end
+        self.AV = 0.0
+        self.profits = np.array([])
+        self.weights = np.zeros((self.n_assets,))
         self.prev_action = np.ones(self.n_assets) / self.n_assets
-        self.current_step = 0
         self.date = self.initial_date
+        self.current_step = 0
         self.balance = self.initial_balance
         return self._get_observation(self.date), self._get_info()
 